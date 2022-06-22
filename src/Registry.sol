b5809f8d234f3e890c203213d379b6ff74958d76
parseb <petra306@protonmail.com>
2022-06-23 00:53:02 +0300

    adds 0.8 univ2 fork

--- src/Registry.sol
+++ src/Registry.sol
@@ -1,15 +1,15 @@
-pragma solidity 0.8.15;
+pragma solidity >=0.8.0;
 
 import "solmate/tokens/ERC20.sol";
 import "solmate/auth/Owned.sol";
-import "uniswapv2-solc0.8/UniswapV2Router02.sol";
+import "uniswapv2-solc0.8/UniswapV2Router.sol";
 import "uniswapv2-solc0.8/UniswapV2Factory.sol";
 
 
 contract Registry is 
     ERC20("A0Z.VC", "A0Z", 18),
-    UniswapV2Factory("10"), //@todo deploy and link in constructor via interface
-    UniswapV2Router02(address(this), address(0)),
+    UniswapV2Factory(address(this)),
+    UniswapV2Router(address(this), address(0)), //import code or call via interface
     Owned(msg.sender)
      {
 
