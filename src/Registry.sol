ce58b7544437a87c2c3e7fc2b26c5a12c694f5c6
parseb <petra306@protonmail.com>
2022-06-23 00:30:37 +0300

    drop

--- src/Registry.sol
+++ src/Registry.sol
@@ -1,21 +1,20 @@
-pragma solidity 0.8.12;
+pragma solidity 0.8.15;
 
 import "solmate/tokens/ERC20.sol";
 import "solmate/auth/Owned.sol";
-import "v2-periphery/UniswapV2Router02.sol";
-import "v2-core/UniswapV2Factory.sol";
+import "uniswapv2-solc0.8/UniswapV2Router02.sol";
+import "uniswapv2-solc0.8/UniswapV2Factory.sol";
+
 
 contract Registry is 
     ERC20("A0Z.VC", "A0Z", 18),
-    UniswapV2Factory("10"),
+    UniswapV2Factory("10"), //@todo deploy and link in constructor via interface
     UniswapV2Router02(address(this), address(0)),
     Owned(msg.sender)
      {
 
-
-        constructor(address _weth, address _chairperson){
-            WETH = _weth;
-            owenr = _chairperson;
+        constructor(address _chairperson){
+            owner = _chairperson;
         }
 
 
